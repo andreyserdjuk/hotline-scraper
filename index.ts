16e26571c212ba43341cf2f84d4e33316d6412f8
andreyserdjuk <ase6120@gmail.com>
2016-11-15 00:59:35 +0200

    parse hotline cat list

---
+++ index.ts
@@ -0,0 +1,20 @@
+import fetch = require('node-fetch');
+import cheerio = require('cheerio');
+import fs = require('fs');
+
+let result = [];
+
+fetch('http://hotline.ua/catalog/')
+.then(res => {
+    return res.text();
+})
+.then(body => {
+    let $ = cheerio.load(body);
+    let links = $('.block').filter('.p_r-20').find('a');
+    // let hrefs = [];
+    let hrefs = links.map((k, v) => {
+        return $(v).attr('href').replace(/^(\/|http:\/\/\w+\.ua\/)/, 'http://hotline.ua/');
+    });
+
+    fs.writeFileSync('out.txt', hrefs.toArray().join('\r\n'));
+});
